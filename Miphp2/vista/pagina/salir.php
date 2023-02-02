47478ecc846c4ad0a1d3ee2305059d055f1c450c
Marisol Salazar <salazarvalenciamarisol@gmail.com>
2023-02-02 16:58:57 -0500

    changes on log out

--- Miphp2/vista/pagina/salir.php
+++ Miphp2/vista/pagina/salir.php
@@ -5,15 +5,3 @@ if(session_destroy()){
     }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Salir</title>
-</head>
-<body>
-    <h1 class="text-center">Salir</h1>
-</body>
-</html>
